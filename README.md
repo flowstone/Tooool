c7a394f9c3f04617d21261b6226799132681a22d
小码农薛尧 <xueyao.me@gmail.com>
2024-10-22 15:26:12 +0800

    批量修改文件名-添加替换功能

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Tooool
 自用的小工具
+
+目前采用PySimpleGUI开发
+
+1. 测试脚本 
+``` bash
+python 脚本.py
+```
+2. 打包脚本
+``` bash
+pyinstaller -F -w 脚本.py
+```
+
+#### BatchFileRenamer 
+
+支持批量修改文件名，前缀、后缀、替换操作
+
+![BatchFileRenamer](img/batch_file_renamer.png)
--- batchFileRename/batch_file_renamer.py
+++ batchFileRename/batch_file_renamer.py
@@ -1,5 +1,7 @@
 import os
 import PySimpleGUI as sg
+import tkinter as tk
+from PIL import Image
 
 class FileRenamerApp:
     def __init__(self):
@@ -7,9 +9,16 @@ class FileRenamerApp:
         menu_def = [
             ['帮助', ['关于']]
         ]
-
-     
         sg.set_options(font=("黑体", 12))
+
+        # 图片路径
+        #image_path = "img/backgroud.png"
+        #image = Image.open(image_path)
+        #original_width, original_height = image.size
+        # 假设我们想将宽度设置为300像素，同时保持宽高比
+        #new_width = 480
+        #new_height = int(original_height * (new_width / original_width))
+        #image_element = sg.Image(filename = image_path,size=(new_width,new_height))
 
         self.layout = [
             [sg.Menu(menu_def)],
@@ -19,9 +28,17 @@ class FileRenamerApp:
             [sg.Input(key='-PREFIX-')],
             [sg.Text("文件名后缀：")],
             [sg.Input(key='-SUFFIX-')],
-            [sg.Button("开始修改"), sg.Button("退出")]
+            [sg.Text("查找字符：")],
+            [sg.Input(key='-CHAR_TO_FIND-')],
+            [sg.Text("替换字符：")],
+            [sg.Input(key='-REPLACE_CHAR-')],
+            [sg.Text("Author：xueyao.me@gmail.com", font=("楷体", 11), text_color='blue')],
+            [sg.Text("Github：https://github.com/flowstone/Tooool", font=("楷体", 11), text_color='blue')],
+            [sg.Button("开始修改"), sg.Button("退出")],
+
         ]
-        self.window = sg.Window("文件批量修改工具", self.layout)
+        self.window = sg.Window("文件名批量修改工具", self.layout)
+
 
     def run(self):
         while True:
@@ -34,22 +51,31 @@ class FileRenamerApp:
                 folder_path = values['-FOLDER_PATH-']
                 prefix = values['-PREFIX-']
                 suffix = values['-SUFFIX-']
+                charToFind = values['-CHAR_TO_FIND-']
+                replaceChar = values['-REPLACE_CHAR-']
                 if folder_path:
-                    self.rename_files(folder_path, prefix, suffix)
+                    self.rename_files(folder_path, prefix, suffix, charToFind, replaceChar)
                     sg.popup("批量文件名修改完成！", title = "提示")
                 else:
                     sg.popup("请选择要修改的文件夹！", title = "警告")
         self.window.close()
 
-    def rename_files(self, folder_path, prefix, suffix):
+    # 修改文件名
+    def rename_files(self, folder_path, prefix, suffix, charToFind, replaceChar):
+        # 遍历文件夹下的文件名
         for filename in os.listdir(folder_path):
             old_path = os.path.join(folder_path, filename)
+            # 判断是否是文件
             if os.path.isfile(old_path):
                 new_filename = f"{prefix}{filename}{suffix}"
+                # 判断是否需要进行文件替换操作
+                if charToFind and replaceChar:
+                    # 替换字符
+                    new_filename = new_filename.replace(charToFind, replaceChar)
                 new_path = os.path.join(folder_path, new_filename)
                 os.rename(old_path, new_path)
 
-
+   
 
 if __name__ == '__main__':
     app = FileRenamerApp()
